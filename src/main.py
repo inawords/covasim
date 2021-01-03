cbf2994cf61c532e9f7394c7987d1acc21ae40e2
jko <johannes.koch@hotmail.com>
2021-01-03 10:28:00 +0100

    Added discussion scenario

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from src.scenarios.current_situation.scenarios import *
 from src.scenarios.current_situation.interventions import INT_CURRENT_SITUATION
 from src.scenarios.best_worst_case.scenarios import *
 from src.scenarios.work_vs_community.scenarios import *
+from src.scenarios.discussion.scenarios import *
 
 overview_plots = [
             'cum_infections',
@@ -28,10 +29,10 @@ def vorarlberg_scenario():
         sim=sim,
         basepars=basepars,
         metapars=scenario_metapars,
-        scenarios=work_community_scenario(**(MIXED['time_influence'][0])))
+        scenarios=SCEN_DISCUSSION)
 
     scenario.run(verbose=True)
-    # scenario.to_excel(filename=result_file_xlsx)
+    scenario.to_excel(filename=result_file_xlsx)
     scenario.plot(do_show=True, to_plot=overview_plots, sep_figs=True)
 
 
---
+++ src/scenarios/discussion/scenarios.py
@@ -0,0 +1,42 @@
+from src.scenarios.work_vs_community.interventions import *
+from src.scenarios.best_worst_case.interventions import *
+
+INTERVENTION_START = "2020-11-01"
+
+SCEN_DISCUSSION = {
+    'best_case': {
+        'name': 'discussion best',
+        'pars': {
+            'interventions': INT_BEST_CASE
+        },
+    },
+
+    'worst_case': {
+        'name': 'discussion worst',
+        'pars': {
+            'interventions': INT_WORST_CASE
+        },
+    },
+
+    'home_office': {
+        'name': 'discussion home office',
+        'pars': {
+            'interventions': create_interventions_home_office(0.5, 0.5, INTERVENTION_START)
+        },
+    },
+
+    'social_distancing': {
+        'name': 'discussion social distancing',
+        'pars': {
+            'interventions': create_interventions_social_distancing(0.8, 0.5, INTERVENTION_START)
+        },
+    },
+
+    'mixed': {
+        'name': 'discussion mixed',
+        'pars': {
+            'interventions': create_interventions_mixed(0.5, 0.5, INTERVENTION_START)
+        },
+    },
+}
+
